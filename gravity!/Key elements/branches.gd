eee05018a45c565bf97f5dc2716b744bf0bc7ad8
SamuelC88888 <24087@burnside.school.nz>
2026-05-28 11:44:56 +1200

    28/05/2026

--- gravity!/Key elements/branches.gd
+++ gravity!/Key elements/branches.gd
@@ -1,6 +1,8 @@
 extends StaticBody2D
-var health = 3
-var death = 0
+const maxhealth = 3
+var health = maxhealth
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -13,11 +15,13 @@ func _process(delta: float) -> void:
 
 func _on_body_entered(body: Node2D) -> void:
 	if body is apple:
-		if death == 3:
-			get_tree().change_scene_to_file("res://Levels/start_screens.tscn")
-		else:
-				death += 1
-				
-		await get_tree().create_timer(0.01).timeout
-		queue_free()
-		pass
+		damage()
+		death()
+
+func damage():
+	health -= 1
+
+
+func death():
+	if health <= 0:
+		get_tree().change_scene_to_file("res://Levels/start_screens.tscn")
