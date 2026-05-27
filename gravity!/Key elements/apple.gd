b9a017678f769e79df13fd3fcfc65c1ed0ed5b8e
SamuelC88888 <24087@burnside.school.nz>
2026-05-27 12:45:41 +1200

    trying to add health in

--- gravity!/Key elements/apple.gd
+++ gravity!/Key elements/apple.gd
@@ -27,7 +27,12 @@ func _physics_process(delta: float) -> void:
 
 
 
+
 	
 
 
 	move_and_slide()
+
+
+
+		
--- gravity!/Key elements/branches.gd
+++ gravity!/Key elements/branches.gd
@@ -1,6 +1,6 @@
 extends StaticBody2D
-
-
+var health = 3
+var death = 0
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -13,7 +13,11 @@ func _process(delta: float) -> void:
 
 func _on_body_entered(body: Node2D) -> void:
 	if body is apple:
+		if death == 3:
+			get_tree().change_scene_to_file("res://Levels/start_screens.tscn")
+		else:
+				death += 1
+				
 		await get_tree().create_timer(0.01).timeout
-		
 		queue_free()
-		
+		pass
