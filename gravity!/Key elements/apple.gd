32ed0d03f716e5d20a594cc1cb03036a09e754dc
SamuelC88888 <24087@burnside.school.nz>
2026-06-09 14:37:52 +1200

    Adjusting hp
    Its what it sounds like

--- gravity!/Key elements/apple.gd
+++ gravity!/Key elements/apple.gd
@@ -3,11 +3,11 @@ class_name apple
 
 const SPEED = 650
 const JUMP_VELOCITY = -400.0
-var maxhp = 1000
-var current_hp = 1000
+var maxhp = 8000
+var current_hp = 8000
 var vdamage = velocity.y
 var speed_int 
-
+signal branch_break
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -29,7 +29,7 @@ func _physics_process(delta: float) -> void:
 	move_and_slide()
 
 func _process(delta: float) -> void:
-	speed_int = int(velocity.y * 0.2)
+	speed_int = int(velocity.y)
 
 
 
@@ -38,6 +38,7 @@ func _on_area_2d_area_entered(area: Area2D) -> void:
 		current_hp -= speed_int 
 		current_hp = clamp(current_hp,0,maxhp)
 		print("ouch! Hp =:",current_hp)
-		print("int",speed_int)
+		print("damage ",speed_int)
 		if current_hp <= 0:
 			get_tree().change_scene_to_file("res://Levels/start_screens.tscn")
+		emit_signal("branch_break")
--- gravity!/Key elements/branches.gd
+++ gravity!/Key elements/branches.gd
@@ -16,3 +16,7 @@ func _ready() -> void:
 #func _on_body_entered(body: Node2D) -> void:
 	#if body is apple:
 		#get_tree().change_scene_to_file("res://Levels/start_screens.tscn")
+
+
+func _on_apple_branch_break() -> void:
+	queue_free()
